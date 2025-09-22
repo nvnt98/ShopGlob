b98e60b76c2e8a5dfef09c86224a175745332ac7
Navneet Gond <navneetgondka@gmail.com>
2025-09-23 02:19:02 +0530

    Updated UI

--- client/src/components/product/ShowProduct.jsx
+++ client/src/components/product/ShowProduct.jsx
@@ -1,69 +1,57 @@
-// ShowProduct.jsx
+// src/components/product/ShowProduct.jsx
 import React, { useContext } from "react";
 import AppContext from "../../context/AppContext";
 import { Link } from "react-router-dom";
 
 const ShowProduct = () => {
-  const { products, filterData, addToCart } = useContext(AppContext); //destructuring the products from context
+  const { filterData, addToCart } = useContext(AppContext);
 
   return (
-    <>
-      <div className="container d-flex justify-content-center align-items-center ">
-        <div className="row container d-flex justify-content-center align-items-center my-5 ">
-          {filterData?.map((product) => (
-            <div
-              key={product._id}
-              className=" my-3 col-md-4 d-flex justify-content-center align-items-center "
-            >
-              <div
-                className="card bg-dark text-light text-center"
-                style={{ width: "18rem" }}
+    <div className="container">
+      <div className="row my-4">
+        {filterData?.map((product) => (
+          <div
+            key={product._id}
+            className="my-3 col-6 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center"
+          >
+            <div className="card bg-dark text-light text-center p-2">
+              <Link
+                to={`/product/${product._id}`}
+                className="d-flex justify-content-center align-items-center p-3"
               >
-                <Link
-                  to={`/product/${product._id}`}
-                  className="d-flex justify-content-center align-items-center p-3 "
-                >
-                  <img
-                    src={product.imgSrc}
-                    className="card-img-top"
-                    alt="..."
-                    style={{
-                      width: "200px",
-                      height: "200px",
-                      borderRadius: "10px",
-                      border: "2px solid yellow",
-                    }}
-                  />
-                </Link>
-                <div className="card-body">
-                  <h5 className="card-title">{product.title}</h5>
-                  <div className="my-3">
-                    <button className="btn btn-primary mx-3">
-                      {product.price}
-                      {" ₹"}
-                    </button>
-                    <button
-                      onClick={() => {
-                        addToCart(
-                          product._id,
-                          product.title,
-                          product.price,
-                          product.qty,
-                          product.imgSrc
-                        );
-                      }}
-                      className="btn btn-warning"
-                    >
-                      Add To Cart
-                    </button>
-                  </div>
+                <img
+                  src={product.imgSrc}
+                  className="card-img-top"
+                  alt={product.title}
+                />
+              </Link>
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <div className="my-2 d-flex justify-content-center gap-2">
+                  <button className="btn btn-primary">
+                    ₹ {product.price}
+                  </button>
+                  <button
+                    className="btn btn-warning"
+                    onClick={() =>
+                      addToCart(
+                        product._id,
+                        product.title,
+                        product.price,
+                        1,
+                        product.imgSrc
+                      )
+                    }
+                  >
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
